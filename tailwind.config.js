556a3f662f08b8831578fabe62da7deb6f5a30e9
Faizan1421 <faizantayyab37@gmail.com>
2025-01-13 19:10:07 +0500

    up

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customGray: "#0e0e0e17", // Name it as you like
+      },
+    },
+  },
+  plugins: [],
+}
